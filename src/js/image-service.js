ea91cccc5bac56c431842d8bb9a6b68d9723c3e7
Farid Suleymanov <105884209+FaridMK@users.noreply.github.com>
2022-09-21 18:54:45 +0400

    1111

--- src/js/image-service.js
+++ src/js/image-service.js
@@ -1,5 +1,8 @@
-import { axios } from '../index.js';
 
+
+import { axios } from '../index.js';
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '30078306-2ba5f79c85dc6ca60b6a24118';
 
 export default class ImageService {
   constructor() {
@@ -8,19 +11,27 @@ export default class ImageService {
     this.simpleLightbox = null;
   }
 
-  async function fetchImages(name, page, perPage) {
-    const BASE_URL = 'https://pixabay.com/api/';
-    const KEY = '30078306-2ba5f79c85dc6ca60b6a24118';
+  async fetchImages() {
+    
+    try {
     const url = `${BASE_URL}?key=${KEY}&q=${this.searchingImg}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${this.page}`;
 
-   try {
-    const response = await axios.get(
-      `${baseURL}?key=${key}&q=${name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`
-    );
-    return response.data;
+    const { data } = await axios.get(url);
+    this.page += 1;
+    return data;
   } catch (error) {
     console.log('ERROR: ' + error);
   }
+
+  resetPage() {
+    this.page = 1;
+  }
+
+  get query() {
+    return this.searchingImg;
+  }
+
+  set query(newSearchingImg) {
+    this.searchingImg = newSearchingImg;
+  }
 }
-  
-  export { fetchImages };
